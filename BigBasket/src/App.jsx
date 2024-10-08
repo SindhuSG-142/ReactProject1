7ad92ab010269700c2d8cbcb0e197101787bb8cf
Sindhu S G <149196469+SindhuSG-142@users.noreply.github.com>
2024-10-08 13:19:05 +0530

    code added for the Homepage

--- BigBasket/src/App.jsx
+++ BigBasket/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import {AllRoutes} from "./components/AllRoutes"
 import './App.css'
 
 function App() {
 
   return (
-    <>
-     
-    </>
+   <div className="App">
+    <AllRoutes/>
+   </div>
   )
 }
 
--- BigBasket/src/main.jsx
+++ BigBasket/src/main.jsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+// import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store";
+import { AuthContextProvider } from "./components/Context/AuthContext";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+     <BrowserRouter>
+      <ChakraProvider>
+        <Provider store={store}>
+          <AuthContextProvider>
+            <App />
+          </AuthContextProvider>
+        </Provider>
+      </ChakraProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
+// reportWebVitals();
