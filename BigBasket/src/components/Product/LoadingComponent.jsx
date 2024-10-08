3c94fc40c17a9a1167ab3538aa278f2f9859ca78
Sindhu S G <149196469+SindhuSG-142@users.noreply.github.com>
2024-10-08 13:26:04 +0530

    code added for product page

---
+++ BigBasket/src/components/Product/LoadingComponent.jsx
@@ -0,0 +1,23 @@
+
+
+import { Skeleton, SkeletonCircle, SkeletonText, Stack } from '@chakra-ui/react'
+const LoadingComp = () => {
+  return (
+    <div>
+    <Stack>
+  <Skeleton height='15px' />
+  <Skeleton height='15px' />
+  <Skeleton height='15px' />
+  <Skeleton height='15px' />
+  <Skeleton height='15px' />
+  <Skeleton height='15px' />
+  <Skeleton height='15px' />
+  <Skeleton height='15px' />
+  <Skeleton height='15px' />
+</Stack>
+      
+    </div>
+  )
+}
+
+export default LoadingComp
